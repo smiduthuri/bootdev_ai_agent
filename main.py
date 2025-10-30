8e6c7333bee62890e79a4ff9669b545061c3fb9d
Sai Miduthuri <saiparthasarathy.m@gmail.com>
2025-10-29 23:51:27 -0700

    Add genai function call schema for get_files_info

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from functions.get_files_info import get_files_info, schema_get_files_info
+
 
 load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
@@ -21,23 +23,44 @@ def parse_args():
     return parser.parse_args()
 
 
-def main():
+if __name__ == "__main__":
     args = parse_args()
     client = genai.Client(api_key=API_KEY)
     model = "gemini-2.0-flash-001"
 
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     messages = [
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)])
     ]
-    response = client.models.generate_content(model=model, contents=messages)
 
-    print(response.text)
+    response = client.models.generate_content(
+        model=model,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt
+        ),
+        contents=messages,
+    )
+
+    print(f"Response text:\n{response.text}")
+    if response.function_calls:
+        print(f"Response function calls:\n{response.function_calls}")
 
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-
-if __name__ == "__main__":
-    main()
